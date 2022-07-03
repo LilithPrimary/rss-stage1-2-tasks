d600e92897520a720a0504d99a4abc219670f426
Denna <lilithprimary@gmail.com>
2022-07-03 10:29:25 +0200

    feat: change sources.js to sources.ts

---
+++ news-JS/src/components/view/sources/sources.ts
@@ -0,0 +1,22 @@
+import './sources.css';
+import  { ISources } from '../../types/sources'
+
+class Sources {
+    draw(data: ISources[]) {
+        const fragment = document.createDocumentFragment();
+        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+
+        data.forEach((item) => {
+            const sourceClone = sourceItemTemp.content.cloneNode(true) as Element;
+
+            (sourceClone.querySelector('.source__item-name') as HTMLSpanElement).textContent = item.name;
+            (sourceClone.querySelector('.source__item') as HTMLDivElement).setAttribute('data-source-id', item.id);
+
+            fragment.append(sourceClone);
+        });
+
+        (document.querySelector('.sources') as HTMLDivElement).append(fragment);
+    }
+}
+
+export default Sources;
