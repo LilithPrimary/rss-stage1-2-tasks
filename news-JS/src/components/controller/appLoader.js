f3ab3bc0c4c5a310b1499e7112f8ec64a9e4d983
Denna <lilithprimary@gmail.com>
2022-07-03 10:22:36 +0200

    feat: add api key to appLoader.js

--- news-JS/src/components/controller/appLoader.js
+++ news-JS/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '16451734face4271928932b75c8a594e', // получите свой ключ https://newsapi.org/
         });
     }
 }
