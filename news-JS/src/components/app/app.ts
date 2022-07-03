8d6e86fdc5d9b9a986cc6af5c575cd96642395b1
Denna <lilithprimary@gmail.com>
2022-07-03 10:34:18 +0200

    feat: change app.js to app.ts

---
+++ news-JS/src/components/app/app.ts
@@ -0,0 +1,16 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import  { IDrawNewsData, IDrawSourcesData } from '../types/appview';
+
+class App {
+    controller = new AppController();
+    view = new AppView();
+
+    start() {
+        (document.querySelector('.sources') as HTMLDivElement)
+            .addEventListener('click', (e) => this.controller.getNews(e, (data: IDrawNewsData) => this.view.drawNews(data)));
+        this.controller.getSources((data: IDrawSourcesData) => this.view.drawSources(data));
+    }
+}
+
+export default App;
