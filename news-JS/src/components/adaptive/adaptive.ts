e73a63949cac04decd5f4d631a45992547725bc3
Denna <lilithprimary@gmail.com>
2022-07-04 18:56:56 +0300

    fix: adaptive - add shadow

--- news-JS/src/components/adaptive/adaptive.ts
+++ news-JS/src/components/adaptive/adaptive.ts
@@ -2,14 +2,25 @@ export function adaptive(): void {
   const menu = document.querySelector(".sources") as HTMLDivElement;
   const burger = document.querySelector(".header__burger") as HTMLDivElement;
   const buttons = document.querySelectorAll(".source__item") as NodeListOf<HTMLDivElement>;
-  console.log(buttons);
+  const header = document.querySelector("header") as HTMLDivElement; 
   burger.addEventListener("click", () => {
     menu.classList.toggle("sources--open");
     burger.classList.toggle("header__burger--open");
+    document.body.classList.toggle("block");
+    header.classList.toggle("header--open");
   })
   buttons.forEach(el => el.addEventListener("click", () => {
-    console.log("click");
+    close();
+  }))
+  document.addEventListener("click", (e: Event)  => {
+    if (menu.classList.contains("sources--open") && (<HTMLDivElement>e.target).tagName === "HEADER") {
+      close();
+    }
+  })
+  function close():void {
     menu.classList.remove("sources--open");
     burger.classList.remove("header__burger--open");
-  }))
+    document.body.classList.remove("block");
+    header.classList.remove("header--open");
+  }
 }
